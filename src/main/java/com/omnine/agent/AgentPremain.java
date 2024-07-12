8cf7cfe63a3f088afdb365a2a0b1cc5f81ec06f5
Mingfa Ma <mingfa.ma@deepnetsecurity.com>
2024-07-12 17:00:28 +0100

    refactor class

--- src/main/java/com/omnine/agent/AgentPremain.java
+++ src/main/java/com/omnine/agent/AgentPremain.java
@@ -1,12 +1,29 @@
 package com.omnine.agent;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.util.jar.JarFile;
+// this class is registered as the Premain-Class in the MANIFEST.MF of this jar
+//
+// this class should have minimal dependencies since it will live in the system class loader while
+// the rest will live in the bootstrap class loader
+public class AgentPremain {
 
-public class NanoAgent {
+    private AgentPremain() {}
     private static CPUMonitor cpuMonitor;
 
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("MyAgent is running");
+
+        File jarFile = new File("/path/to/yourjarfile.jar");
+        try {
+            inst.appendToBootstrapClassLoaderSearch(new JarFile(jarFile));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         cpuMonitor = new CPUMonitor();
         cpuMonitor.startMonitoring();
 
